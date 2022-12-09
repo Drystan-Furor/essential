a80b3c96d656477ea31ac52ad88dbb9575b5164a
Tristan Arts <80721880+Drystan-Furor@users.noreply.github.com>
2022-12-09 13:01:56 +0100

    Update StoryProgram

--- src/main/java/javabasics/storyprogram/StoryProgram.java
+++ src/main/java/javabasics/storyprogram/StoryProgram.java
@@ -5,21 +5,18 @@ import java.util.Scanner;
 public class StoryProgram {
     public static void main(String[] args) {
         //   1. Get the adjective.
-        String adjective = "";
         System.out.println("ADJECTIVE: bright / hot / snowy / misty / other:... ?");
         // Get user input
         Scanner scanner = new Scanner(System.in);
-        adjective = scanner.nextLine();
+        String adjective = scanner.nextLine();
 
         //   2. Get the season.
-        String season = "";
         System.out.println("Name a season: ?");
-        season = scanner.nextLine();
+        String season = scanner.nextLine();
 
         //   3. Get the number.
-        int amount = 0;
         System.out.println("Name a number: ?");
-        amount = scanner.nextInt();
+        int amount = scanner.nextInt();
 
         //   4. Display results.
         System.out.println("On a " + adjective + " " + season +
